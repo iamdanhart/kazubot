ceb3647b82408677811fbe6374272834fa502818
Danny H <iamdannydarko@gmail.com>
2020-08-23 15:36:54 -0400

    Changes to start script

--- src/main/resources/start.sh
+++ src/main/resources/start.sh
@@ -1,1 +1,5 @@
-java -Ddiscord.token=${DISCORD_TOKEN} -jar target/Kazubot-1.0-SNAPSHOT.jar
+#!/bin/bash
+
+java -Ddiscord.token=${DISCORD_TOKEN} \
+     -Dxivapi.token=${XIVAPI_TOKEN} \
+     -jar target/Kazubot-1.0-SNAPSHOT.jar
